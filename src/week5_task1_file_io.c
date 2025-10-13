e524739f8e73dfaa4f5d1d614417e99d08c9a38f
Mightymob0303 <realmightymob@gmail.com>
2025-10-13 08:38:37 +0000

    week5

--- src/week5_task1_file_io.c
+++ src/week5_task1_file_io.c
@@ -10,18 +10,35 @@ int main(void) {
     FILE *fp;
     char filename[100] = "data.txt";
     char line[256];
-
+    int line_count = 0;
     // TODO: 1. Open file for writing (mode = "w")
+    printf("Enter filename to write to (e.g., data.txt): ");
+    scanf("%99s", filename);
+    
+    fp = fopen(filename, "w");
     // TODO: 2. Check if file opened successfully
+    if (fp == NULL){
+        perror("error cant open file properly");
+        return 1;
+    }
+    printf("Writing lines to %s...\n", filename);
     // TODO: 3. Write 2–3 lines of text to the file using fprintf()
+    fprintf(fp, "hello, there kind sir,C!\n");
+    fprintf(fp, " i hope you are having a fine day today C!\n");
     // TODO: 4. Close the file
-
+    fclose(fp);
     // TODO: 5. Open file again for reading (mode = "r")
+    fp = fopen(filename, "r");
     // TODO: 6. Use fgets() in a loop to read and print each line to the console
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+        line_count++;
+    }
     // TODO: 7. Close the file
-
+    fclose(fp);
     // BONUS: ask user for filename instead of using default "data.txt"
+    
     // BONUS: count number of lines read
-
+    printf("\nTotal lines read: %d\n", line_count);
     return 0;
 }
--- src/week5_task2_struct_save_load.c
+++ src/week5_task2_struct_save_load.c
@@ -28,22 +28,41 @@ int main(void) {
     const char *filename = "student.txt";
 
     // TODO: Call save_student() to save student data to file
+    printf("saving student to file");
+    save_student(s1, filename);
+    
     // TODO: Call load_student() to read data back into a new struct
+    printf("loading student from file");
+    Student s2 = load_student(filename);
     // TODO: Print loaded data to confirm correctness
+printf("loaded student: %s, %d, GPA %.2F\n", s2.name, s2.age, s2.gpa );
 
     return 0;
 }
 
 // TODO: Implement save_student()
-// Open file for writing, check errors, write fields, then close file
 void save_student(Student s, const char *filename) {
-    // ...
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL) {
+        perror("Error opening file for writing");
+        exit(1);
+    }
+// Open file for writing, check errors, write fields, then close file
+    fprintf(fp, "%s %d %.2f\n", s.name, s.age, s.gpa);
+
+    fclose(fp);
 }
 
 // TODO: Implement load_student()
+
 // Open file for reading, check errors, read fields, then close file
 Student load_student(const char *filename) {
     Student s;
-    // ...
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file for reading");
+        exit(1);
+    }
+    fclose(fp);
     return s;
 }
